904a102e63c7941323c02da0100ee72a277857d3
cameronoremac <cameron_mil@hotmail.com>
2018-05-09 13:29:47 -0400

    added IIFE to app.js

---
+++ Scripts/app.js
@@ -0,0 +1,13 @@
+/* Custom JS goes here*/
+
+//IIFE - Immediately Invoked Function Expression
+//anonymous self executing function
+(function(){
+
+    function Start() {
+        console.log("App Started!")
+        console.log("Hello, World!")
+    }
+    
+    window.onload = Start;
+})()
